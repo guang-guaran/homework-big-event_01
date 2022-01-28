1e2a201f55e0cd6593abc69403910f4ec7477693
guaran <2833198046@qq.com>
2022-01-28 22:29:20 +0800

    修复bug

--- assets/js/public.js
+++ assets/js/public.js
@@ -5,3 +5,12 @@ let layer = layui.layer
 
 // 配置全局路径
 axios.defaults.baseURL = 'http://www.liulongbin.top:3007'
+
+// 设置请求拦截器 每次请求都需要这是请求头
+axios.interceptors.request.use(function (config) {
+  // console.log(config)
+  config.headers.Authorization = localStorage.getItem('token')
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
